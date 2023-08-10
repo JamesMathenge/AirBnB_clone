3d8970214805acd1d9eaa190e352ac9fc2d9d6ca
Apiyo120 <apiyodianah@gmail.com>
2023-08-10 13:11:40 -0700

    AirBnB

---
+++ console.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+"""Module for entry point of the command interpreter."""
+
+import cmd
+from models import storage
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the AirBnB clone project."""
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """Exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the prorm."""
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line."""
+        pass
+
+    def help_quit(self):
+        """Help message for the quit command."""
+        print("Quit command to exit the program")
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, save it and print the id."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Print the string representation of an instance."""
+        args = args.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            obj_dict = storage.all()
+            key = "{}.{}" .format(args[0], args[1])
+            if key in obj_dict:
+                print(obj_dict[key])
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Print all strins representations of instances."""
+        obj_dict = storage.all()
+        if not arg:
+            print([str(obj) for obj in obj_dict.values()])
+        elif arg in BaseModel.__sunclasses__():
+            print([
+                str(obj)
+                for key, obj in obj_dict.items()
+                if key.startwith(arg)
+                ])
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id."""
+        args = arg.split()
+        obj_dict = storage.all()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in obj_dict:
+            print("** no instance found **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            instance = obj_dict[key]
+            attr_name = args[2]
+            attr_value = args[3]
+            setattr(instance, attr_name, attr_value)
+            instance.save()
+
+
+if __name__ == '__name__':
+    HBNBCommand().cmdloop()
